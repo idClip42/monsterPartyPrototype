34d6ba4223bd1f83dbc17dbd274851727a81494e
Alexander Earley <axe6801@rit.edu>
2025-03-09 19:42:32 -0400

    Formatting updates from the robot

--- Assets/Scripts/Monster/Simple/SimpleMonster.cs
+++ Assets/Scripts/Monster/Simple/SimpleMonster.cs
@@ -25,18 +25,18 @@ public class SimpleMonster : MonoBehaviour
     {
         _characters = FindObjectsByType<Character>(FindObjectsSortMode.None);
 
-        if(_eye == null)
+        if (_eye == null)
             throw new System.Exception("Missing eye.");
 
         _navManager = FindFirstObjectByType<NavigationManager>();
-        if(_navManager == null)
+        if (_navManager == null)
             throw new System.Exception($"Null _navManager on {this.gameObject.name}");
 
         _navMeshAgent = GetComponent<NavMeshAgent>();
-        if(_navMeshAgent == null)
+        if (_navMeshAgent == null)
             throw new System.Exception($"Null nav mesh agent on {this.gameObject.name}");
 
-        if(_minRedirectTime > _maxRedirectTime)
+        if (_minRedirectTime > _maxRedirectTime)
             throw new System.Exception("Invalid redirect times");
     }
 
@@ -47,13 +47,13 @@ public class SimpleMonster : MonoBehaviour
 
     void Update()
     {
-        if(_eye == null)
+        if (_eye == null)
             throw new System.Exception("Missing eye.");
 
         RaycastHit hitInfo;
         Vector3 offset = Vector3.up * 1.5f;
-        foreach(var target in _characters){
-
+        foreach (var target in _characters)
+        {
             Vector3 targetPos = target.transform.position + offset;
             Vector3 toTarget = targetPos - _eye.transform.position;
             float distance = toTarget.magnitude;
@@ -70,11 +70,11 @@ public class SimpleMonster : MonoBehaviour
             // If we hit something
             // and what we hit was the target character,
             // still a clear line of sight.
-            if(!lineOfSight && hitInfo.collider.gameObject == target.gameObject)
+            if (!lineOfSight && hitInfo.collider.gameObject == target.gameObject)
                 lineOfSight = true;
 
-            if(lineOfSight){
-
+            if (lineOfSight)
+            {
                 Vector3 lightDirection = _eye.transform.forward;
                 Vector3 targetDirection = toTarget / distance;
                 float spotAngle = _eye.spotAngle;
@@ -82,14 +82,17 @@ public class SimpleMonster : MonoBehaviour
                 float spotAngleInRadians = spotAngle * Mathf.Deg2Rad;
                 float cosHalfSpotAngle = Mathf.Cos(spotAngleInRadians / 2);
                 float dotProduct = Vector3.Dot(lightDirection, targetDirection);
-                if (dotProduct > cosHalfSpotAngle) {
+                if (dotProduct > cosHalfSpotAngle)
+                {
                     // Target is within the vision cone
                     Debug.DrawLine(
                         _eye.transform.position,
                         targetPos,
                         Color.red
                     );
-                } else {
+                }
+                else
+                {
                     // Target is outside the vision cone
                     Debug.DrawLine(
                         _eye.transform.position,
@@ -98,7 +101,8 @@ public class SimpleMonster : MonoBehaviour
                     );
                 }
             }
-            else {
+            else
+            {
                 Debug.DrawLine(
                     _eye.transform.position,
                     hitInfo.point,
@@ -108,10 +112,11 @@ public class SimpleMonster : MonoBehaviour
         }
     }
 
-    private void NewDestination(){
-        if(_navManager == null)
+    private void NewDestination()
+    {
+        if (_navManager == null)
             throw new System.Exception($"Null _navManager on {this.gameObject.name}");
-        if(_navMeshAgent == null)
+        if (_navMeshAgent == null)
             throw new System.Exception($"Null nav mesh agent on {this.gameObject.name}");
 
         _navMeshAgent.SetDestination(
