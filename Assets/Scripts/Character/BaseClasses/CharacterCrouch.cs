51b97da21e99bf883a311887bc032269eae08361
Alexander Earley <axe6801@rit.edu>
2025-03-09 11:35:18 -0400

    Crouching stuff moved to abstraction

--- Assets/Scripts/Character/BaseClasses/CharacterCrouch.cs
+++ Assets/Scripts/Character/BaseClasses/CharacterCrouch.cs
@@ -2,5 +2,33 @@ using UnityEngine;
 
 public abstract class CharacterCrouch : MonoBehaviour
 {
-    public abstract bool IsCrouching { get; }
+    private Character _characterBase;
+
+    private bool _isCrouching = false;
+    public bool IsCrouching => _isCrouching;
+
+    void Awake()
+    {
+        _characterBase = GetComponent<Character>();
+        if(_characterBase == null)
+            throw new System.Exception($"Null character base on {this.gameObject.name}");
+    }
+
+    void Update()
+    {
+        if(_characterBase.State == State.Player){
+            if(Input.GetButtonDown("Crouch")){
+                ToggleCrouch();
+            }
+        }
+    }
+
+    private void ToggleCrouch(){
+        this._isCrouching = !this._isCrouching; 
+        if(this._isCrouching) EnableCrouch();
+        else DisableCrouch();
+    }
+
+    protected abstract void EnableCrouch();
+    protected abstract void DisableCrouch();
 }
--- Assets/Scripts/Character/Simple/SimpleCharacterCrouch.cs
+++ Assets/Scripts/Character/Simple/SimpleCharacterCrouch.cs
@@ -2,40 +2,21 @@ using UnityEngine;
 
 public class SimpleCharacterCrouch : CharacterCrouch
 {
-    private SimpleCharacter _characterBase;
-
     [SerializeField]
     [Range(0.1f, 0.7f)]
     private float _crouchHeightPercentage = 0.5f;
 
-    private bool _isCrouching = false;
-    public override bool IsCrouching => _isCrouching;
+    protected override void EnableCrouch() => ToggleCrouch(true);
 
-    void Awake()
-    {
-        _characterBase = GetComponent<SimpleCharacter>();
-        if(_characterBase == null)
-            throw new System.Exception($"Null character base on {this.gameObject.name}");
-    }
+    protected override void DisableCrouch() => ToggleCrouch(false);
 
-    void Update()
-    {
-        if(_characterBase.State == State.Player){
-            if(Input.GetButtonDown("Crouch")){
-                ToggleCrouch();
-            }
-        }
-    }
-
-    private void ToggleCrouch(){
-        this._isCrouching = !this._isCrouching; 
-
+    private void ToggleCrouch(bool isCrouching){
         float meshHeight = GetComponent<MeshRenderer>().bounds.extents.y;
         float startYScale = transform.localScale.y;
         float newHeight;
         float newYScale;
 
-        if(this._isCrouching){
+        if(isCrouching){
             newYScale = startYScale * _crouchHeightPercentage;
             newHeight = meshHeight * _crouchHeightPercentage;
         }
@@ -52,7 +33,7 @@ public class SimpleCharacterCrouch : CharacterCrouch
 
         transform.position += new Vector3(
             0,
-            (newYScale - startYScale) / 2,
+            (newHeight - meshHeight) / 2,
             0
         );
     }
