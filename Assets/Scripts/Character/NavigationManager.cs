644f94c5d6495c14a74bb1fb19cd10c2b5e84ef3
Alexander Earley <axe6801@rit.edu>
2025-03-09 19:24:30 -0400

    Navigation Manager now collects all viable nav mesh points

--- Assets/Scripts/Character/NavigationManager.cs
+++ Assets/Scripts/Character/NavigationManager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using NUnit.Framework;
 using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,10 +8,15 @@ using UnityEngine.AI;
 
 public class NavigationManager : MonoBehaviour
 {
+    private const float INTERVAL = 3;
+
     [SerializeField]
     private NavMeshSurface? _standingNavMesh;
     [SerializeField]
     private NavMeshSurface? _crouchingNavMesh;
+
+    private Vector3[] _standingNavPoints = {};
+    private Vector3[] _crouchingNavPoints = {};
 
     public int StandingAgentTypeId { get {
         if(_standingNavMesh == null)
@@ -28,5 +35,34 @@ public class NavigationManager : MonoBehaviour
             throw new System.Exception("Null _standingNavMesh");
         if(_crouchingNavMesh == null)
             throw new System.Exception("Null _crouchingNavMesh");
+
+        _standingNavPoints = GetPossiblePointsOnNavMesh(_standingNavMesh);
+        _crouchingNavPoints = GetPossiblePointsOnNavMesh(_crouchingNavMesh);
+    }
+
+    private Vector3[] GetPossiblePointsOnNavMesh(NavMeshSurface navMeshSurface){
+        List<Vector3> tempList = new List<Vector3>();
+        NavMeshHit hit;
+        Bounds bounds = navMeshSurface.navMeshData.sourceBounds;
+        for(float x = bounds.min.x; x <= bounds.max.x; x += INTERVAL){
+            for(float y = bounds.min.y; y <= bounds.max.y; y += INTERVAL){
+                for(float z = bounds.min.z; z <= bounds.max.z; z += INTERVAL){
+                    Vector3 testPosition = new Vector3(x,y,z);
+                    bool foundOne = NavMesh.SamplePosition(
+                        testPosition,
+                        out hit,
+                        INTERVAL / 2,
+                        new NavMeshQueryFilter(){
+                            agentTypeID=navMeshSurface.agentTypeID,
+                            areaMask=NavMesh.AllAreas
+                        }
+                    );
+                    if(foundOne){
+                        tempList.Add(hit.position);
+                    }
+                }
+            }
+        }
+        return tempList.ToArray();
     }
 }
