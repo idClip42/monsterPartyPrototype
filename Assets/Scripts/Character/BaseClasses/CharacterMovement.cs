2167f39682defcc7067a4b3ced3a3c62752af6a1
Alexander Earley <axe6801@rit.edu>
2025-03-16 15:57:18 -0400

    Drawing velocity lines

--- Assets/Scripts/Character/BaseClasses/CharacterMovement.cs
+++ Assets/Scripts/Character/BaseClasses/CharacterMovement.cs
@@ -3,4 +3,15 @@ using UnityEngine;
 public abstract class CharacterMovement : MonoBehaviour
 {
     public abstract Vector3 CurrentVelocity { get; }
+
+    void OnDrawGizmosSelected()
+    {
+        if(CurrentVelocity != Vector3.zero){
+            Debug.DrawLine(
+                transform.position,
+                transform.position + CurrentVelocity,
+                Color.white
+            );
+        }
+    }
 }
