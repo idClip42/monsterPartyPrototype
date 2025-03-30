1e16ddc2c91a1095b5d61e3850bf60feab626b4f
Alexander Earley <axe6801@rit.edu>
2025-03-30 09:42:23 -0400

    Footsteps!

---
+++ Assets/Scripts/Character/Simple/SimpleCharacterFootsteps.cs
@@ -0,0 +1,61 @@
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+#nullable enable
+
+[RequireComponent(typeof(AudioSource))]
+public class SimpleCharacterFootsteps : MonoBehaviour {
+    [SerializeField]
+    private AudioClip[] _footstepClips = {};
+
+    [SerializeField]
+    [Range(0.5f, 5)]
+    private float _stepRate = 0.7f;
+
+    [SerializeField]
+    [Range(1,10)]
+    private float _maxVolumeSpeed = 5;
+
+    private Character? _character;
+    private AudioSource? _audioSource;
+    private float _stepTimer = 0;
+    private int _currentClipIndex = 0;
+
+    void Awake()
+    {
+        _character = GetComponentInParent<Character>();
+        if(_character == null)
+            throw new System.Exception("Missing character");
+
+        _audioSource = GetComponent<AudioSource>();
+        if(_audioSource == null)
+            throw new System.Exception("Missing footsteps audio source");
+    }
+
+    void Update()
+    {
+        if(_character == null)
+            throw new System.Exception("Missing character");
+        if(_audioSource == null)
+            throw new System.Exception("Missing footsteps audio source");
+
+        _stepTimer += Time.deltaTime;
+
+        float speed = this._character.CurrentVelocity.magnitude;
+
+        _audioSource.volume = Mathf.InverseLerp(0, _maxVolumeSpeed, speed);
+
+        float maxTimer = 1 / (speed * _stepRate);
+        if(_stepTimer >= maxTimer){
+            _audioSource.Stop();
+
+            _audioSource.clip = _footstepClips[_currentClipIndex];
+            if(_audioSource.clip == null)
+                throw new System.Exception("Null audio clip");
+            _audioSource.Play();
+
+            _stepTimer = 0;
+            _currentClipIndex = (_currentClipIndex + 1) % _footstepClips.Length;
+        }
+    }
+}
