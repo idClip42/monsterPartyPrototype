50b55357505e7dc7030983f37185bc2929dff232
Alexander Earley <axe6801@rit.edu>
2025-04-21 19:43:06 -0400

    Removed unused Usings

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEditor;
-using System;
 using System.Collections.Generic;
 
 #nullable enable
--- Assets/Scripts/IDebugInfoProvider.cs
+++ Assets/Scripts/IDebugInfoProvider.cs
@@ -1,6 +1,6 @@
-#nullable enable
-
 using System.Collections.Generic;
+
+#nullable enable
 
 namespace MonsterParty
 {
